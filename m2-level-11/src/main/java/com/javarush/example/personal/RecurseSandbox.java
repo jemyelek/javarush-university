14b646fcfb80865852cddc5aab24c652b75b19ac
Jemshit <jimy.jw@gmail.com>
2026-01-15 20:06:48 +0500

    All new classes from lectures

---
+++ m2-level-11/src/main/java/com/javarush/example/personal/RecurseSandbox.java
@@ -0,0 +1,20 @@
+package com.javarush.example.personal;
+
+import java.io.File;
+
+public class RecurseSandbox {
+    public static void main(String[] args) {
+        String tab = "";
+        printAll(new File("V:\\Program Files"), tab);
+    }
+
+    private static void printAll(File files, String tab) {
+        for (File file : files.listFiles()) {
+            if (file.isDirectory()) {
+                System.out.println(tab + file.getName() + "\\...");
+                printAll(file, tab + "\t|");
+            } else
+                System.out.println(tab + "-" + file.getName());
+        }
+    }
+}
